75a0c396222babcd252570fea2a77e83b0772b7c
valentinerutto <rutto28@gmail.com>
2015-08-23 13:09:45 +0300

    method that takes in a block

--- blocks.rb
+++ blocks.rb
@@ -1,3 +1,4 @@
+#passing blocks to a method
 def doItTwice
   yield
   yield
@@ -5,4 +6,12 @@ end
 
 doItTwice do
     puts 'i passed this block to the method'
+end
+#method that takes a block,we turn the block into a proc with &
+def doittwice(&block)
+    block.call
+    block.call
+end
+doittwice do 
+    puts "I LOVE RUBY ON RAILS"
 end
