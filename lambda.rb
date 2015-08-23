e551dac4977eac46f4693319b637d5550320976a
valentinerutto <rutto28@gmail.com>
2015-08-23 15:44:22 +0300

    created proc method with 2 returns

--- lambda.rb
+++ lambda.rb
@@ -1,5 +1,9 @@
 #checks the num of args it receives and send an arg error if it doesnt match
 l=lambda{"i am a lambda"}
 puts l.call
-l.call('arg')
-#returns 
+#l.call('arg'),this proves the point on args
+#return diffrence of lambda and proc
+def proc
+    proc.new{return 2/2}.call
+        return 4/2
+    end
